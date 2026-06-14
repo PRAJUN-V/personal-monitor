2847510178f16d875f69602af59a9e783f0621c7
PRAJUN V <prajunvelandi@gmail.com>
2026-06-14 11:36:18 +0530

    Fix: Remove unused and incorrect create_url import from database.py

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_url, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
